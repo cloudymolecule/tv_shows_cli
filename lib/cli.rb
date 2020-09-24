2a3c725c703480c2fdc4f53939eb92d8b1fd55d8
Cloudymolecule <baires82@gmail.com>
2020-09-24 17:58:52 -0400

    working on print_show_details

--- lib/cli.rb
+++ lib/cli.rb
@@ -6,7 +6,7 @@ class CLI #=> responsible for user interaction
         puts ""
         puts "WELCOME TO TV SHOWS FINDER"
         puts "__________________________"
-        @api_date = CLI.date_acquire
+        @api_date = date_acquire
         API.grab_shows(@api_date)
         puts ""
         shows = Show.all
@@ -14,28 +14,32 @@ class CLI #=> responsible for user interaction
         puts "Type a show number to get more info, or type 'exit' to exit the application."
         input = gets.strip.downcase
         while input != "exit" do
-
+            if input.to_i <= Show.all.count && input.to_i != 0 && input.to_i >= 1
+                print_show_details(input)
+                #puts a.show_sum
+                input = gets.strip.downcase.to_i
+            end
         end
     end
 
-    def self.date_acquire #=> gets date and formats it for API
+    def date_acquire #=> gets date and formats it for API
         puts ""
         puts "Please enter a year between 1950 and #{Time.new.year}:"
         input_y = gets.strip
         until input_y.length == 4 && input_y.to_i != 0 && input_y.to_i >= 1950 && input_y.to_i <= Time.new.year do
             puts ""
-            puts "Incorrect format. Please enter a year between 1950 and #{Time.new.year}:"
+            puts "Incorrect year or format. Please enter a year between 1950 and #{Time.new.year}:"
             input_y = gets.strip   
         end
 
         puts "Please enter a month:"
         input_m = gets.strip.downcase
-        input_m = CLI.month_check(input_m)
+        input_m = month_check(input_m)
         until input_m.to_i >= 1 && input_m.to_i <= 12
             puts ""
             puts "Incorrect format. Please enter a month:"
             input_m = gets.strip.downcase
-            input_m =CLI.month_check(input_m)    
+            input_m = month_check(input_m)    
         end
         
         puts "Please enter a day of the month:"
@@ -54,7 +58,7 @@ class CLI #=> responsible for user interaction
         api_date = "#{input_y}-#{input_m}-#{input_d}"
     end
 
-    def self.month_check(string)
+    def month_check(string) #=> validates the month's input
         month = ""
         case string
         when "january"
@@ -105,5 +109,13 @@ class CLI #=> responsible for user interaction
         puts ""
     end
 
-end
+    def print_show_details(show)
+        sh_summary = Show.all[show.to_i - 1].show_sum.split("<b>").join("").split("<p>").join("").split("</p>").join("").split("</b>").join("").split("</i>").join("").split("<i>").join("")
+        #I'm going to use gsub instead ^^
+        puts ""
+        puts sh_summary
+        puts ""
+        #binding.pry
+    end
 
+end
