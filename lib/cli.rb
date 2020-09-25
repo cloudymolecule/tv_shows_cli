d1a6574368948eb1369d498ea11130f12b461f8b
Cloudymolecule <baires82@gmail.com>
2020-09-25 12:57:05 -0400

    now you can go back to show list

--- lib/cli.rb
+++ lib/cli.rb
@@ -11,14 +11,19 @@ class CLI #=> responsible for user interaction
         puts ""
         shows = Show.all
         print_shows(shows)
-        puts "Type a show number to get more info, or type 'exit' to exit the application."
+        puts "TYPE A SHOW NUMBER TO GET MORE INFO, OR TYPE 'EXIT' TO EXIT THE APPLICATION."
         input = gets.strip.downcase
         while input != "exit" do
-            if input == "info"
-                #binding.pry
+            if input == "back"
+                print_shows(shows)
+                puts "TYPE A SHOW NUMBER TO GET MORE INFO, OR TYPE 'EXIT' TO EXIT THE APPLICATION."
                 input = gets.strip.downcase
             elsif input.to_i <= Show.all.count && input.to_i != 0 && input.to_i >= 1
-                print_show_details(input)
+                print_show_summary(input)
+                puts "TYPE:"
+                puts "'EPISODE' to get info on that specific episode"
+                puts "'CAST' to see the show's cast"
+                puts "'BACK' to go back to the show's list for #{date_normalizer}"
                 input = gets.strip.downcase
             end
             
@@ -28,29 +33,29 @@ class CLI #=> responsible for user interaction
 
     def date_acquire #=> gets date and formats it for API
         puts ""
-        puts "Please enter a year between 1950 and #{Time.new.year}:"
+        puts "PLEASE ENTER A YEAR BETWEEN 1950 AND #{Time.new.year}:"
         input_y = gets.strip
         until input_y.length == 4 && input_y.to_i != 0 && input_y.to_i >= 1950 && input_y.to_i <= Time.new.year do
             puts ""
-            puts "Incorrect year or format. Please enter a year between 1950 and #{Time.new.year}:"
+            puts "INCORRECT YEAR OR FORMAT. PLEASE ENTER A YEAR BETWEEN 1950 AND #{Time.new.year}:"
             input_y = gets.strip   
         end
 
-        puts "Please enter a month:"
+        puts "PLEASE ENTER A MONTH:"
         input_m = gets.strip.downcase
         input_m = month_check(input_m)
         until input_m.to_i >= 1 && input_m.to_i <= 12
             puts ""
-            puts "Incorrect format. Please enter a month:"
+            puts "INCORRECT FORMAT. PLEASE ENTER A MONTH:"
             input_m = gets.strip.downcase
             input_m = month_check(input_m)    
         end
         
-        puts "Please enter a day of the month:"
+        puts "PLEASE ENTER A DAY OF THE MONTH:"
         input_d = gets.strip
         until input_d.length >= 1 && input_d.length <= 2  && input_d.to_i != 0 && input_d.to_i <= 31
             puts ""
-            puts "Incorrect format. Please enter a day of the month:"
+            puts "INCORRECT FORMAT. PLEASE ENTER A DAY OF THE MONTH:"
             input_d = gets.strip   
         end
         if input_m.length == 1
@@ -95,18 +100,28 @@ class CLI #=> responsible for user interaction
         month
     end
 
-    def print_shows(shows) #=> puts the TV show and the episode aired that day
+    def date_normalizer
         n_date = @api_date.split("-")
         normal_date = []
         normal_date << n_date[1]
         normal_date << n_date[2]
         normal_date << n_date[0]
         normal_date = normal_date.join("/")
+        normal_date
+    end
+
+    def print_shows(shows) #=> prints a list of the the TV shows and the episodes aired that day
+        # n_date = @api_date.split("-")
+        # normal_date = []
+        # normal_date << n_date[1]
+        # normal_date << n_date[2]
+        # normal_date << n_date[0]
+        # normal_date = normal_date.join("/")
         puts ""
         puts "------------------------------------------------------------"
-        puts "Here's a list of all TV shows aired on #{normal_date} in the USA"
-        ago = Time.new.year - n_date[0].to_i
-        puts "That was #{ago} years ago, time sure flies!"
+        puts "HERE'S A LIST OF ALL TV SHOWS AIRED ON #{date_normalizer} IN THE USA"
+        ago = Time.new.year - date_normalizer.split("/")[2].to_i
+        puts "THAT WAS #{ago} YEARS AGO, TIME SURE FLIES!"
         puts "------------------------------------------------------------"
         puts ""
         shows.each.with_index(1) do |s, i|
@@ -115,14 +130,13 @@ class CLI #=> responsible for user interaction
         puts ""
     end
 
-    def print_show_details(show)
-        sh_summary = Show.all[show.to_i - 1].show_sum.gsub!(/<("[^"]*"|'[^']*'|[^'">])*>/, "")
+    def print_show_summary(show) #=> puts a specific show's summary
+        sh_summary = Show.all[show.to_i - 1].show_sum.gsub(/<("[^"]*"|'[^']*'|[^'">])*>/, "")
         puts "-------------"
         puts "SHOW SUMMARY:"
         puts "-------------"
         puts sh_summary
         puts ""
-        
     end
 
 end
